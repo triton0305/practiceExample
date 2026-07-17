367aa911250e1959525d049c325ddc39b6c05317
triton0305 <99065149+triton0305@users.noreply.github.com>
2026-07-18 03:29:11 +0900

    [level 3] Title: 숫자 게임, Time: 48.99 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/3/12987. 숫자 게임/숫자 게임.py
@@ -4,7 +4,7 @@ def solution(A, B):
     answer = 0
     a = 0
     b = 0
-    while a < len(A) and b < len(B):
+    for i in range(0, len(B)):
         if A[a] < B[b]:
             a += 1
             b += 1
