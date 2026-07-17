7b0ec7bf158bd7c08537343ec0c05623d1f83047
triton0305 <99065149+triton0305@users.noreply.github.com>
2026-07-18 04:46:37 +0900

    [level 3] Title: 야근 지수, Time: 291.14 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,18 +1,15 @@
 import heapq
 def solution(n, works):
     answer = 0
-    sum = 0
-    tem = 0
+    total = 0
     for i in range(0,len(works)):
-        sum+=works[i]
+        total+=works[i]
         works[i] *= -1
     heapq.heapify(works)
-    if sum > n:
+    if total > n:
         while (n > 0) :
-            tem = works[0]
-            tem += 1
-            heapq.heappop(works)
-            heapq.heappush(works, tem)
+            temp = heapq.heappop(works) + 1
+            heapq.heappush(works, temp)
             n -= 1
         for j in range (0, len(works)):
             answer += pow(works[j], 2)
