40dc7d530e95ac98da532f26752fe151bc7197ee
triton0305 <99065149+triton0305@users.noreply.github.com>
2026-07-25 00:10:48 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.11 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.c
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.c
@@ -2,26 +2,23 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-int compare(const void * a,const void * b);
+int compare_up(const void * a,const void * b);
+int compare_down(const void * a, const void * b);
 int solution(int A[], size_t A_len, int B[], size_t B_len) {
     int answer = 0;
-    int tem = 0;
-
-    qsort(A, A_len, sizeof(int), compare);
-    qsort(B, B_len, sizeof(int), compare);
-    for (int i = 0 ; i < (B_len/2); i++)
-    {
-        tem = B[i];
-        B[i] = B[B_len-i-1];
-        B[B_len-i-1] = tem;
-    }
+    qsort(A, A_len, sizeof(int), compare_up);
+    qsort(B, B_len, sizeof(int), compare_down);
     for (int j = 0; j<A_len; j++)
     {
         answer += A[j]*B[j];
     }
     return answer;
 }
-int compare(const void * a, const void * b)
+int compare_up(const void * a, const void * b)
 {
-    return *(const int *)a - *(const int *)b;
+    return *(int *)a - *(int *)b;
+}
+int compare_down(const void * a, const void * b)
+{
+    return *(int *)b - *(int *)a;
 }
